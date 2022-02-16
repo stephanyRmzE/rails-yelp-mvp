34b35884e2dedbaffcaae1ab5a247d00763b66fc
stephany ramirez <stephany_re@hotmail.com>
2022-02-16 07:42:56 -0700

    second challenge finish

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,8 +8,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to review_path(@review)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
